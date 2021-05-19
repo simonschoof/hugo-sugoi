c3a77d633d276a8c46c6e2c0b25abb955c542729
Simon Schoof <simon.schoof@posteo.net>
2021-05-19 14:30:24 +0200

    hugo theme | #31 | toggle, set and store theme

--- static/js/site.js
+++ static/js/site.js
@@ -1,22 +1,42 @@
 function toggleTheme() {
-    //var prettifyDark = document.getElementById('prettify-dark')
+
+    var theme = localStorage.getItem("theme")
+    // change data-theme
+    if (!theme || theme === "light") {
+        setTheme("dark")
+    }
+    else {
+        setTheme("light")
+    }
+}
+
+function setTheme(theme) {
+
+    var prettifyDark = document.getElementById('prettify-dark')
     var sheets = document.styleSheets
 
     // temporariily set transition class on html element
     document.documentElement.classList.add('transition-theme')
 
-    // change data-theme
-    document.documentElement.setAttribute("data-theme", "dark")
-    //prettifyDark.removeAttribute('disabled')
-    //prettifyDark.disabled = false
-    /*else {
+    if (theme === "dark") {
+        document.documentElement.setAttribute("data-theme", "dark")
+        prettifyDark.removeAttribute('disabled')
+        prettifyDark.disabled = false
+        localStorage.setItem("theme", "dark")
+    }
+    else {
         document.documentElement.removeAttribute("data-theme", "dark")
         prettifyDark.disabled = true
-    }*/
+        localStorage.setItem("theme", "light")
+    }
 
     // remove transition class
     window.setTimeout(function () {
         document.documentElement.classList.remove('transition-theme')
     }, 1000)
-
 }
+
+document.addEventListener("DOMContentLoaded", function() {
+    var theme = localStorage.getItem("theme") || "light"
+    setTheme(theme);
+  });
